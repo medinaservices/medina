cccc452426fabd319896afd900801342471b0a33
mehmetusa <mattunlu@gmail.com>
2026-01-18 13:00:08 -0500

    fix build

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -3,9 +3,6 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.{js,jsx}"],
-    rules: {
-      // your custom rules here
-    },
+    files: ["**/*.{js,jsx}"]
   },
 ];
